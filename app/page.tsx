c15955b3a220533859d5471f8f2e8d946e6753a7
john <johnhowardm@hotmail.com>
2024-07-02 11:37:44 +0100

    Removing borders used to help when coding tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <section className='border border-green-500 py-12'>
-      <div className='container border border-blue-500'>
+    <section className='py-12'>
+      <div className='container'>
         <h1 className='mb-2 text-3xl font-bold'>
           NextJS Starter Template with shadcn/ui and Clerk
         </h1>
